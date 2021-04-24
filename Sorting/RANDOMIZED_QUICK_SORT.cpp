d821a66d2ef67c63d4b9f892a4934ef7a19a8ce1
pranshu200 <pranshuy2020@gmail.com>
2021-04-24 23:09:51 +0530

    Quick Sort and Randomized Quick Sort

---
+++ Sorting/RANDOMIZED_QUICK_SORT.cpp
@@ -0,0 +1,70 @@
+// RANDOMIZED QUICK SORT
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define input(a,n){ for(int i=0;i<n;i++) cin>>a[i];}
+#define output(a,n){ for(int i=0;i<n;i++) cout<<a[i]<<" ";}
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void randomization(int arr[],int l,int h)
+{
+    int n=r-l+1;
+    int temp=rand()%n;
+    swap(&arr[l+temp],&arr[r]);
+}
+
+int Partition(int arr[],int lb,int ub)
+{   
+    int pivot = arr[ub];
+    int i = (lb - 1);
+    for (int j = lb; j <= ub - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[ub]);
+    return (i + 1);
+}
+
+void QuickSrt(int arr[],int lb, int ub)
+{
+    int i,n;
+    n=ub-lb+1;
+    if(lb<ub)
+    {
+        randomization(arr,lb,ub);
+        int loc=Partition(arr,lb,ub);
+        QuickSrt(arr,lb,loc-1);
+        QuickSrt(arr,loc+1,ub);
+    } 
+}
+
+int main() 
+{ 
+    int n;
+    cin>>n;
+    int arr[n];
+    input(arr,n);
+    QuickSrt(arr,0,n-1);
+    output(arr,n);
+    return 0;
+}
+
+/* 	INPUT
+
+15
+43 64 13 78 864 346 786 456 21 19 8 434 76 270 601
+
+	OUTPUT
+
+8 13 19 21 43 64 76 78 270 346 434 456 601 786 864 
+*/
